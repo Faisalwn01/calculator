55b962af7b101bef99b92a8365066fc3e0233df1
Faisalwn01 <faisalwn83@amikom.ac.id>
2023-03-13 16:59:17 +0700

    joki cuy

--- 1.cs
+++ 1.cs
@@ -2,6 +2,14 @@ class calculator
 {
 	static void main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLineine("hasil penambahan={0}+{1}={2}",a,b,penambahan(a,b));
+		Console.WriteLineine("hasil pengurangann={0}-{1}={2}",a,b,pengurangann(a,b));
+		
+		Console.WriteLineine("Tekan sembarang untuk keluar");
+		Console.Readkey();
 		
 	}
 	static int penambahan(int a, int b)
